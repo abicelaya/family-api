3ff1bb4a3a7dbdd4ee849c4ed3da1de05d397609
abicelaya <abicelaya@gmail.com>
2022-05-25 19:40:49 +0000

    datastrutures terminado

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, json
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
@@ -26,17 +26,37 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def handle_get_all_member():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+   
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_member(id):
+    result = jackson_family.get_member(id)
+
+    return jsonify(result), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def handle_add_member():
+    request_body = request.get_jason(request.data)
+    if request_body:
+        jackson_family.add_member(request_body)
+        return jsonify(request_body), 200
+    else:
+        return "Ocurrio un error", 400
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def handle_delete_member(id):
+    if id < len(member):
+        member.pop(id)
+    else:
+        return 'position not found'
+    response = jsonify(member)
+    return response
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
